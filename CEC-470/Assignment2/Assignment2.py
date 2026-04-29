1bcdbc7627d129749255c93e049e5480e63d6273
Joseph Neubert <joeyfneubert@gmail.com>
2026-04-28 21:25:57 -0700

    ADDITION CEC-470: Assignment 4 Endianness Code

---
+++ CEC-470/Assignment2/Assignment2.py
@@ -0,0 +1,26 @@
+"""
+CEC-470 Assignment 4
+Author: Joseph Neubert
+Description: Systen endianness (LE) demostration with my PC.
+"""
+
+import sys
+
+# Determine System Endianness
+if sys.byteorder == "big":
+    print("Big Endian")
+elif sys.byteorder == "little":
+    print("Little Endian")
+else:
+    print("Unknown")
+
+# Demonstrate System Endianness with a String
+testString = "CEC470"
+charList = []
+for i in testString:
+    x = hex(ord(i))
+    charList.insert(0, x)
+    print(f"{x} - {i}")
+
+stringList = str(charList)
+print(stringList)
